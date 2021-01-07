bcd27f81e495db6d6d5f9e43334c236d959311e0
veronicatofino <veronicatofino@gmail.com>
2021-01-07 11:04:19 -0500

    Adding first css selecetor

--- protractor/headless.config.ts
+++ protractor/headless.config.ts
@@ -2,8 +2,9 @@ import { Config, browser } from 'protractor';
 import { reporter } from './helpers/reporter';
 export const config: Config = {
   framework: 'jasmine',
-  specs: ['../test/google.spec.js'],
+  specs: ['../test/**/*.spec.js'],
   SELENIUM_PROMISE_MANAGER: false,
+  getPageTimeout: 1000,
   onPrepare: () => {
     browser.ignoreSynchronization = true;
     reporter();
