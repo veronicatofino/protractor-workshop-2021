c1494a2b25bc21128fb58eaf466fa916490ecba6
veronicatofino <veronicatofino@gmail.com>
2021-01-11 13:32:31 -0500

    Given When Then estructure

--- test/buy-tshirt.spec.ts
+++ test/buy-tshirt.spec.ts
@@ -5,29 +5,40 @@ import { MenuContentPage, ProductsListPage,
         ShippingStepPage, PaymentStepPage,
         BankPaymentPage, OrderSummaryPage } from '../src/page';
 
-describe('Buy a t-shirt', () => {
-  const menuContentPage: MenuContentPage = new MenuContentPage();
-  const productsListPage: ProductsListPage = new ProductsListPage();
-  const productAddedPage: ProductAddedPage = new ProductAddedPage();
-  const summaryPage: SummaryStepPage = new SummaryStepPage();
-  const signInPage: SignInStepPage = new SignInStepPage();
-  const addressStepPage: AddressStepPage = new AddressStepPage();
-  const shippingStepPage: ShippingStepPage = new ShippingStepPage();
-  const paymentStepPage: PaymentStepPage = new PaymentStepPage();
-  const bankPaymentPage: BankPaymentPage = new BankPaymentPage();
-  const orderSummaryPage: OrderSummaryPage = new OrderSummaryPage();
-  it('Then should be bought a t-shirt', async () => {
+describe('Given a clothing page', async () => {
+  beforeAll(async () => {
     await browser.get('http://automationpractice.com/');
-    await menuContentPage.goToTShirtMenu();
-    await productsListPage.selectTShirt();
-    await productAddedPage.goToSummaryStepPage();
-    await summaryPage.goToSignInStepPage();
-    await signInPage.goToAddressStepPage('vtofino@gmail.com', 'WorkshopProtractor');
-    await addressStepPage.goToShippingStepPage();
-    await shippingStepPage.acceptTerms();
-    await shippingStepPage.goToPaymentStepPage();
-    await paymentStepPage.goToBankOption();
-    await bankPaymentPage.goToOrderSummaryPage();
-    await orderSummaryPage.checkOrderConfirmation();
+  });
+  describe('When want to buy a T Shirt', async () => {
+    beforeAll(async () => {
+      const menuContentPage: MenuContentPage = new MenuContentPage();
+      const productsListPage: ProductsListPage = new ProductsListPage();
+      const productAddedPage: ProductAddedPage = new ProductAddedPage();
+      const summaryPage: SummaryStepPage = new SummaryStepPage();
+      await menuContentPage.goToTShirtMenu();
+      await productsListPage.selectTShirt();
+      await productAddedPage.goToSummaryStepPage();
+      await summaryPage.goToSignInStepPage();
+    });
+    describe('And login to the page', async () => {
+      const signInPage: SignInStepPage = new SignInStepPage();
+      await signInPage.goToAddressStepPage('vtofino@gmail.com', 'WorkshopProtractor');
+
+      describe('And select the default address', async () => {
+        const addressStepPage: AddressStepPage = new AddressStepPage();
+        await addressStepPage.goToShippingStepPage();
+        const shippingStepPage: ShippingStepPage = new ShippingStepPage();
+        await shippingStepPage.acceptTerms();
+
+        it('Then pay for the order with bank option', async () => {
+          const paymentStepPage: PaymentStepPage = new PaymentStepPage();
+          await paymentStepPage.goToBankOption();
+          const bankPaymentPage: BankPaymentPage = new BankPaymentPage();
+          await bankPaymentPage.goToOrderSummaryPage();
+          const orderSummaryPage: OrderSummaryPage = new OrderSummaryPage();
+          await orderSummaryPage.checkOrderConfirmation();
+        });
+      });
+    });
   });
 });
