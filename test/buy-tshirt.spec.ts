df220beaec7ab056b2fe785aba69aaadcf00463f
veronicatofino <veronicatofino@gmail.com>
2021-01-12 07:25:21 -0500

    Better structure

--- test/buy-tshirt.spec.ts
+++ test/buy-tshirt.spec.ts
@@ -5,16 +5,16 @@ import { MenuContentPage, ProductsListPage,
         ShippingStepPage, PaymentStepPage,
         BankPaymentPage, OrderSummaryPage } from '../src/page';
 
-describe('Given a clothing page', async () => {
+describe('Given a shopping page', async () => {
   beforeAll(async () => {
     await browser.get('http://automationpractice.com/');
   });
   describe('When want to buy a T Shirt', async () => {
+    const menuContentPage: MenuContentPage = new MenuContentPage();
+    const productsListPage: ProductsListPage = new ProductsListPage();
+    const productAddedPage: ProductAddedPage = new ProductAddedPage();
+    const summaryPage: SummaryStepPage = new SummaryStepPage();
     beforeAll(async () => {
-      const menuContentPage: MenuContentPage = new MenuContentPage();
-      const productsListPage: ProductsListPage = new ProductsListPage();
-      const productAddedPage: ProductAddedPage = new ProductAddedPage();
-      const summaryPage: SummaryStepPage = new SummaryStepPage();
       await menuContentPage.goToTShirtMenu();
       await productsListPage.selectTShirt();
       await productAddedPage.goToSummaryStepPage();
@@ -22,21 +22,28 @@ describe('Given a clothing page', async () => {
     });
     describe('And login to the page', async () => {
       const signInPage: SignInStepPage = new SignInStepPage();
-      await signInPage.goToAddressStepPage('vtofino@gmail.com', 'WorkshopProtractor');
-
+      beforeAll(async () => {
+        await signInPage.goToAddressStepPage('vtofino@gmail.com', 'WorkshopProtractor');
+      });
       describe('And select the default address', async () => {
         const addressStepPage: AddressStepPage = new AddressStepPage();
-        await addressStepPage.goToShippingStepPage();
         const shippingStepPage: ShippingStepPage = new ShippingStepPage();
-        await shippingStepPage.acceptTerms();
-
-        it('Then pay for the order with bank option', async () => {
+        beforeAll(async () => {
+          await addressStepPage.goToShippingStepPage();
+          await shippingStepPage.acceptTerms();
+          await shippingStepPage.goToPaymentStepPage();
+        });
+        describe('And pay for the order with bank option', async () => {
           const paymentStepPage: PaymentStepPage = new PaymentStepPage();
-          await paymentStepPage.goToBankOption();
           const bankPaymentPage: BankPaymentPage = new BankPaymentPage();
-          await bankPaymentPage.goToOrderSummaryPage();
-          const orderSummaryPage: OrderSummaryPage = new OrderSummaryPage();
-          await orderSummaryPage.checkOrderConfirmation();
+          beforeAll(async () => {
+            await paymentStepPage.goToBankOption();
+            await bankPaymentPage.goToOrderSummaryPage();
+          });
+          it('Then the buy should be complete', async () => {
+            const orderSummaryPage: OrderSummaryPage = new OrderSummaryPage();
+            await orderSummaryPage.checkOrderConfirmation();
+          });
         });
       });
     });
